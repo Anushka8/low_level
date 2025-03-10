4e35e222eabe3afe7439e249c62ad5f212b67327
anush <ac2224@rit.edu>
2025-03-10 11:15:23 -0400

    operator overload and dynamic array

---
+++ tutorial.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std; // to avoid std::cout everytime
+/*******************************************************************************************************************
+HELLO WORLD
+*******************************************************************************************************************/
+void hello_world()
+{
+    int x;
+    cout << "Hello, world!" << endl;
+    cin >> x;
+    cout << x + 86 << endl;
+}
+
+/*******************************************************************************************************************
+POINTERS
+*******************************************************************************************************************/
+void pointers()
+{
+    int num = 10;
+    int *ptr = &num; // ptr is a pointer to an int and stores the address of num
+
+    cout << "Value of num: " << num << endl;
+    cout << "Value stored in pointer: " << ptr << endl;
+    cout << "Address of num: " << &num << endl;
+    // *ptr means deferencing, allowing actual value stored at the address
+    std::cout << "Value at address stored in pointer: " << *ptr << endl;
+
+    *ptr = 20;
+    cout << "Updated num: " << num << endl;
+}
+
+/*******************************************************************************************************************
+OPERATOR OVERLOAD
+*******************************************************************************************************************/
+
+class Test
+{
+    int x, y;
+
+public:
+    Test() { cout << "Constructor is called\n"; }
+    ~Test() { cout << "Destructor is called\n"; }
+
+    void *operator new(size_t size)
+    {
+        cout << "Allocating " << size << " bytes\n";
+        return malloc(size);
+    }
+
+    void operator delete(void *ptr) noexcept
+    {
+        cout << "Freeing memory\n";
+        free(ptr);
+    }
+};
+
+/*******************************************************************************************************************
+DYNAMIC ARRAY
+*******************************************************************************************************************/
+class DynamicArray
+{
+private:
+    int *arr;
+    size_t capacity;
+    size_t size;
+
+public:
+    // constructor with capacity as a parameter, followed for initializer (no significant effect)
+    DynamicArray(size_t cap = 2) : capacity(cap), size(0)
+    {
+        arr = new int[capacity];
+    }
+
+    // if size increases, dynamically increase array size, reassign values to bigger array
+    void push_back(int val)
+    {
+        if (capacity == size)
+        {
+            capacity *= 2;
+            int *newArr = new int[capacity];
+            for (size_t i = 0; i < size; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = newArr;
+        }
+        arr[size++] = val;
+    }
+
+    int operator[](size_t index) const
+    {
+        return arr[index];
+    }
+
+    ~DynamicArray()
+    {
+        delete[] arr;
+    }
+};
+
+int main()
+{
+    // hello_world();
+
+    // pointers();
+
+    // Test *obj = new Test();
+    // delete obj;
+
+    DynamicArray arr;
+    arr.push_back(4);
+    arr.push_back(5);
+    arr.push_back(6);
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        cout << "Element " << i << " is " << arr[i] << endl;
+    }
+
+    return 0;
+}
